073f2f10da63af2fb72d35495161a7f037ff84e7
wesleyberr <wesleyberrypanda@gmail.com>
2025-11-26 18:17:22 +0100

    Move kick.js to commands/admin directory

---
+++ commands/admin/kick.js
@@ -0,0 +1,9 @@
+// This is the kick command
+
+module.exports = {
+    name: 'kick',
+    description: 'Kick a user from the server',
+    execute(message, args) {
+        // kick logic here
+    }
+};
